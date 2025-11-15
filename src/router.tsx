27a7ee3af178b4b87cdf54b0041f7254ac37619e
eaangrino <eaangrino@gmail.com>
2025-11-15 15:42:37 -0500

    Update router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,28 @@
 import { createHashRouter } from 'react-router-dom';
+import { Layout } from './components';
 import Home from './pages/Home';
+import Portfolio from './components/Portfolio';
+import PortfolioPreview from './components/PortfolioPreview';
 
 export const router = createHashRouter([
 	{
-		path: '/',
-		element: <Home />,
+		path: '',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <Home />,
+			},
+			{
+				path: 'portfolio',
+				element: <Portfolio />,
+				children: [
+					{
+						path: ':projectId',
+						element: <PortfolioPreview />,
+					},
+				],
+			},
+		],
 	},
 ]);
