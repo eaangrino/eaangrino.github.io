6f3ff0e93932d1f814b075da3dec7971f2c55b83
eaangrino <eaangrino@gmail.com>
2025-11-15 15:42:56 -0500

    Update files and add new images

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  preview: {
+    allowedHosts: [
+      "hp15da0011la",
+      "hp15da0011la.local"
+    ],
+    host: '0.0.0.0',
+    cors: true,
+    port: 3173,
+  },
   server: {
-    allowedHosts: [ 'hp15da0011la' ]
-  }
+    allowedHosts: [
+      "hp15da0011la",
+      "hp15da0011la.local"
+    ],
+    watch: { usePolling: true },
+    host: '0.0.0.0',
+    cors: true,
+    port: 2173,
+  },
 })
 
 // https://vite.dev/config/
